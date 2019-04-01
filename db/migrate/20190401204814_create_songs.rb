ac43534d664c1c1e422c1fd90f78bb4576eb8649
John <johnbaironlr@gmail.com>
2019-04-01 15:51:18 -0500

    Música y más música

---
+++ db/migrate/20190401204814_create_songs.rb
@@ -0,0 +1,12 @@
+class CreateSongs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :songs do |t|
+      t.references :album, foreign_key: true
+      t.string :name
+      t.string :number
+      t.string :preview_url
+
+      t.timestamps
+    end
+  end
+end
